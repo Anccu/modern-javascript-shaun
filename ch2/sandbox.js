4c790425432b32c81b88ae5a632f604f075144df
Anccu <jbm.bojorquez@gmail.com>
2022-09-16 22:17:55 +0200

    add variables, constants, comments and data types

--- ch2/sandbox.js
+++ ch2/sandbox.js
@@ -1,1 +1,36 @@
-alert('hello, world!!');
+//alert('hello, world!!');
+console.log(1);
+console.log(2);
+// variables
+let age = 25;
+let year = 2022;
+console.log(age);
+console.log(age, year);
+
+age = 30;
+console.log(age);
+
+const points = 100;
+//points = 50;
+console.log(points);
+
+var score = 75;
+console.log(score);
+
+// one line comment
+
+/* multi
+   line
+   comment
+*/
+
+// DATA TYPES
+/*
+* Numbers -> 1,2,3,...3.14
+* Strings -> 'Hello World'  "mario@mail.com"
+* Boolean -> true / false
+* Null -> Explicitly set a variable with no value
+* Undefined -> For variables that have not yet been defined (sometimes given this value from the browser)
+* Object -> Complex data structures - Arrays, Dates, Literals etc
+* Symbol -> Used with objects (new adition to js)
+*/ 
