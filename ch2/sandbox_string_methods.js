43267b6cce536a86d2fa1ce1b0cdd86e8604ef69
Anccu <jbm.bojorquez@gmail.com>
2022-11-25 20:58:14 +0100

    added some string methods

---
+++ ch2/sandbox_string_methods.js
@@ -0,0 +1,12 @@
+// common string methods
+let email = 'mario@thenetninja.co.uk';
+
+//let result = email.lastIndexOf('n');
+
+//let result = email.slice(0,10);// from position 0 to position 10
+
+//let result = email.substring(4,10);
+
+//let result = email.replace('m','w'); // find the first 'm' and replace it for 'w'
+
+console.log(result);
