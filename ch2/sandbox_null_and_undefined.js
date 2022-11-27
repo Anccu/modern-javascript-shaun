f9535bb4acf21f1179f89e45dd4b47a1d4bd230e
Anccu <jbm.bojorquez@gmail.com>
2022-11-27 10:51:38 +0100

    added undefined and null examples

---
+++ ch2/sandbox_null_and_undefined.js
@@ -0,0 +1,9 @@
+// let age;
+
+// console.log(age, age + 3, `the age is ${age}`); // undefined NaN "the age is undefined"
+
+let age = null;
+
+console.log(age, age + 3, `the age is ${age}`); // null 3 "the age is null"  so null + 3 is 3, interesting!!
+
+
