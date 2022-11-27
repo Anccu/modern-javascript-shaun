0be7de57cd9205c3a92fbefb030833289db3ac72
Anccu <jbm.bojorquez@gmail.com>
2022-11-27 11:20:26 +0100

    added boolean comparisons

---
+++ ch2/sandbox_booleans_and_comparisons.js
@@ -0,0 +1,31 @@
+// booleans & comparisons
+// console.log(true, false, "true","false");
+
+// methods can return booleans
+// let email = 'luigi@thenetninja.co.uk';
+// let names = ['mario','luigi','toad'];
+
+// let result = email.includes('@');
+// let result = names.includes('luigi');
+
+// console.log(result);
+
+// comparison operators
+let age = 25;
+
+// console.log(age == 25); // true
+// console.log(age == 30); // false
+// console.log(age != 30); // true
+// console.log(age > 20); // true
+// console.log(age < 20); // false
+// console.log(age <= 20); // false
+// console.log(age >= 20); // true
+
+
+let name = 'shaun';
+console.log(name == 'shaun'); // true
+console.log(name == 'Shaun'); // false
+console.log(name > 'crystal'); // true - because s > c 
+console.log(name > 'Crystal'); // true - because s > C apparently
+
+
