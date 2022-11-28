b049cb099c8a89233b413f6677f9d170c07f9731
Anccu <jbm.bojorquez@gmail.com>
2022-11-28 21:42:16 +0100

    added type conversion cases

---
+++ ch2/sandbox_type_conversion.js
@@ -0,0 +1,16 @@
+// type conversion
+let score = '100';
+
+// score = Number(score);
+// console.log(score + 1);
+// console.log(typeof score);
+
+// let result = Number('Hello');
+
+// let result = String(50);
+// let result = Boolean(-40); // 0 is false, different than zero is true
+// let result = Boolean('0'); // strings of any value are true
+let result = Boolean(''); // empty strings are false
+console.log(result, typeof result);
+
+
