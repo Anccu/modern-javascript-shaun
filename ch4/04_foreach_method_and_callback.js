6a32dc825e572633fbddf82113934bf6c1ba6d2b
Anccu <jbm.bojorquez@gmail.com>
2022-12-04 18:10:05 +0100

    added foreach methods and callbacks

---
+++ ch4/04_foreach_method_and_callback.js
@@ -0,0 +1,32 @@
+// callbacks & foreach
+
+// we can a function as aparameter and we call it a callback function
+const myFunc = (callbackFunc) => {
+  // do something 
+  let value = 50;
+  callbackFunc(value);
+}
+
+// we can call the function and we can pass the parameter (a function), for example:
+myFunc(value => {
+  // do something 
+  console.log(value);
+}); // 50
+
+console.log('------------------------------------------------------')
+
+let people = ['mario','luigi','ryu','shaun','chun-li'];
+
+
+// the first parameter is the value you are iterating at that moment
+// the second parameter is the index
+people.forEach((person,index) => { 
+  console.log(index,person);
+});
+
+// we can also define the function and call it later
+const logPerson = (person, index) => {
+  console.log(`${index} - hello ${person}`)
+};
+
+people.forEach(logPerson);
