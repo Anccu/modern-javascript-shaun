0d98faa609d5146f63a337dc0e01c75f55d5122f
Anccu <jbm.bojorquez@gmail.com>
2022-12-09 11:16:39 +0100

    added selection by id, class and tag name

---
+++ ch6/02_other_ways_to_query_the_DOM.js
@@ -0,0 +1,36 @@
+// Other ways to query the DOM
+const title = document.getElementById('page-title');
+console.log(title);
+console.log('---------------------------------------------------');
+
+// get elements by their class name
+const errors = document.getElementsByClassName('error');
+console.log(errors); // result a html list
+console.log(errors[0]); // we can retrieve an element by index
+
+/*
+// we cannnot use foreach method with html lists
+errors.forEach(error => {
+  console.log(error); // gives error
+});
+*/
+console.log('---------------------------------------------------');
+
+// get elements by their tag name 
+const paras = document.getElementsByTagName('p');
+console.log(paras);
+console.log(paras[1]);
+console.log('---------------------------------------------------');
+
+/**
+ * HTMLCollection methods
+ * .item()
+ * .namedItem()
+ * 
+ * Node List methods
+ * .item()
+ * .entries()
+ * .forEach()
+ * .keys()
+ * .values()
+ */
